49de0094d2e50f131745ffab126cfbe2738e7658
Blaise Roberts <blaiseroberts@Blaises-MacBook-Pro-2.local>
2017-01-18 09:20:01 -0600

    added conditional check and enterListener

--- main.js
+++ main.js
@@ -6,21 +6,41 @@ var character;
 var tree;
 var spaces;
 
+function clear () {
+	window.location.reload()
+};
+
 function growTree (event) {
-	height = parseInt(document.getElementById("height").value)
+	height = parseFloat(document.getElementById("height").value)
 	character = document.getElementById("character").value
 	var characterCount = 1
 	var spaceCount = height
-	for (var i = 0; i < height; i++) {
-		spaces = " ".repeat(spaceCount);
-		var characterString = character.repeat(characterCount);
-		tree = spaces + characterString;
-		console.log(tree);
-		spaceCount -= 1;
-		characterCount += 2;
+	if (character.length === 1 && height > 0) {	
+		for (var i = 0; i < height; i++) {
+			spaces = " ".repeat(spaceCount);
+			var characterString = character.repeat(characterCount);
+			tree = spaces + characterString;
+			console.log(tree);
+			spaceCount -= 1;
+			characterCount += 2;
+		}
+	} else {
+		alert("Please enter a number greater than 0 for the height and a single character");
+		clear();
 	}
-	
-
 }
 
 growButton.addEventListener("click", growTree);
+
+document.getElementById("character").onkeypress=function(e){
+    if(e.keyCode==13){
+        e.preventDefault();
+        var pressedEnter = growButton.click();
+    }
+}
+document.getElementById("height").onkeypress=function(e){
+    if(e.keyCode==13){
+        e.preventDefault();
+        var pressedEnter = growButton.click();
+    }
+}
