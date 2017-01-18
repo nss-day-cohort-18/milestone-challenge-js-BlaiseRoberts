1e31dd6a67cc2ead8eaa9a351c374f75c7052c84
Blaise Roberts <blaiseroberts@Blaises-MacBook-Pro-2.local>
2017-01-17 18:20:29 -0600

    finished the challenge, needs if/else to verify inputs

--- main.js
+++ main.js
@@ -3,33 +3,24 @@
 var growButton = document.getElementById("growButton")
 var height;
 var character;
-var tree = "";
-var spaces = "";
-var j = 0;
-// for (var j = 0; j < height-i; j++) {
-// 			 spaces = spaces + "-"
-// }
+var tree;
+var spaces;
 
 function growTree (event) {
 	height = parseInt(document.getElementById("height").value)
 	character = document.getElementById("character").value
+	var characterCount = 1
+	var spaceCount = height
 	for (var i = 0; i < height; i++) {
-		height = height-i
-		while (j < height-i) {
-			 spaces += "-";
-			 j++;
-		}
-		console.log(spaces)
-		tree = spaces + character + tree
-		console.log(tree)
-		tree = character + tree
-		
+		spaces = " ".repeat(spaceCount);
+		var characterString = character.repeat(characterCount);
+		tree = spaces + characterString;
+		console.log(tree);
+		spaceCount -= 1;
+		characterCount += 2;
 	}
+	
+
 }
 
-
-
-
-
-
-growButton.addEventListener("click", growTree)
+growButton.addEventListener("click", growTree);
